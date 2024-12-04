e70db55f815926e9d99feb6a69a5200b65eb6885
Yisus1019 <jesusdavidgarciajimenez1019@gmail.com>
2024-12-04 14:58:27 -0600

    Delete Button tarea 1 directory

--- Button tarea 1/prueba.js
+++
@@ -1,7 +0,0 @@
-const para = document.querySelector("button");
-para.addEventListener("click",upDateName);
-
-function upDateName(){
-    let Name = prompt("Escribe un nombre: ");
-    para.textContent = "Player 1:  " + Name;
-}
